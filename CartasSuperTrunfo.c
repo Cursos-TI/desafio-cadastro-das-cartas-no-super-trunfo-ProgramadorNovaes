d898eb7a589516d73fef9822c8eac34b87bb35d6
ProgramadorNovaes <henriquenovaes1589@gmail.com>
2025-02-19 20:15:24 +0000

    passo 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,11 +10,26 @@ char estado,codigo,cidade;
 int populacao,pontosturisticos;
 float area,pib;
 
-printf("Digite uma letra para categoria estado:\n",estado);
+printf("   Digite uma letra para Categoria Estado:\n");
 scanf("%c", &estado);
 
-printf("Digite um numero de 1 a 4: \n",codigo);
+printf("   Digite um numero de 1 a 4: \n");
 scanf("%c", &codigo);
+
+printf("   Digite o nome da cidade: \n");
+scanf("%c", &cidade);
+
+printf("   Digite uma letra para Categoria Estado:\n");
+scanf("%d", &populacao);
+
+printf("   Digite uma letra para Categoria Estado:\n");
+scanf("%d", &pontosturisticos);
+
+printf("   Digite uma letra para Categoria Estado:\n");
+scanf("%f", &area);
+
+printf("   Digite uma letra para Categoria Estado:\n");
+scanf("%f", &pib);
 
 
 
