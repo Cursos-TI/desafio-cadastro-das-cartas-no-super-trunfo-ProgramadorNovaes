e399880166bf609c3963336d260c7daa26b5f7a9
ProgramadorNovaes <henriquenovaes1589@gmail.com>
2025-02-19 20:02:29 +0000

    carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,19 @@ printf("---------------------------------------------\n");
 printf("-      Bem Vindo ao Jogo Super Trunfo       -\n");
 printf("---------------------------------------------\n");
 
+char estado,codigo,cidade;
+int populacao,pontosturisticos;
+float area,pib;
+
+printf("Digite uma letra para categoria estado:\n",estado);
+scanf("%c", &estado);
+
+printf("Digite um numero de 1 a 4: \n",codigo);
+scanf("%c", &codigo);
+
+
+
+
 
 
 
