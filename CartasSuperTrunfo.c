49a19f41737dfd3266dac34e87cece635f9c9b5a
ProgramadorNovaes <henriquenovaes1589@gmail.com>
2025-02-19 21:07:51 +0000

    uma parte foi

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,32 +6,32 @@ printf("---------------------------------------------\n");
 printf("-      Bem Vindo ao Jogo Super Trunfo       -\n");
 printf("---------------------------------------------\n");
 
-char estado,codigo,cidade;
+char codigo;
+char estado[10];
+char cidade[20];
 int populacao,pontosturisticos;
 float area,pib;
 
 printf("   Digite uma letra para Categoria Estado:\n");
-scanf("%c", &estado);
+scanf("    %c", &estado);
 
-
-
-printf("   Digite um numero de 1 a 4: \n");
-scanf("%c", &codigo);
+printf("   Digite a populaçâo: \n");
+scanf("    %d", &populacao);
 
 printf("   Digite o nome da cidade: \n");
-scanf("%c", &cidade);
+scanf("    %c", &cidade);
+
+printf("   Digite o Pib: \n");
+scanf("    %f", &pib);
+
+printf("   Digite uma :\n");
+scanf("    %c", &codigo);
 
 printf("   Digite uma letra para Categoria Estado:\n");
-scanf("%d", &populacao);
+scanf("    %f", &area);
 
 printf("   Digite uma letra para Categoria Estado:\n");
-scanf("%d", &pontosturisticos);
-
-printf("   Digite uma letra para Categoria Estado:\n");
-scanf("%f", &area);
-
-printf("   Digite uma letra para Categoria Estado:\n");
-scanf("%f", &pib);
+scanf("    %d", &pontosturisticos);
 
 
 
